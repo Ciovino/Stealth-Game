8328a98658a7e3b0328fa3a168fb56ed52409ee0
Ciovino <tarantino.giovanbattista01@gmail.com>
2025-02-04 20:38:00 +0100

    added guards

--- source/main.c
+++ source/main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <conio.h> // kbhit()
 #include <time.h> // clock()
-#include "..\header\Screen.h"
 
-#define FPS 5
+#include "..\header\Screen.h"
+#include "..\header\Random.h"
+
+#define FPS 6
 
 // Data found in the PowerShell settings
 #define MAX_W 120
@@ -22,6 +24,14 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define POS(x, y) (x + y*MAX_W)
 
+// Oposite direction have opposite sign
+typedef enum{
+    NORD    = -1, 
+    EAST    = -2, 
+    SOUTH   = 1,
+    WEST    = 2
+}DIRECTIONS;
+
 /*** Stealth Game ***/
 typedef struct{
     char face;
@@ -33,17 +43,36 @@ typedef struct{
 }PLAYER;
 
 typedef struct{
+    char face;
+    int x;
+    int y;
+    int old_x;
+    int old_y;
+
+    DIRECTIONS direction;
+
+    // How many frame it needs to move
+    int speed;
+    int frameCounter;
+}GUARD;
+
+typedef struct{
     int fullSize;
     char* map;
     PLAYER* player;
     
     int deltaTime;
     clock_t thisFrame;
+
+    GUARD** allGuards;
+    int totalGuards;
 }MAP;
 
 MAP* InitializeMap(int width, int heigth);
 void AddPlayer(MAP* map, PLAYER* player);
+void CreateRandomGuards(MAP* map, int totalGuards);
 void UpdateMap(MAP* map);
+int UpdateGuards(MAP* map);
 void PrintMap(MAP* map);
 void FreeMap(MAP* map);
 
@@ -64,6 +93,9 @@ int main(int argc, char **argv)
 
     printf("this u -> %c\n", player->face);
     while (!kbhit()) {}
+
+    // Create some guards
+    CreateRandomGuards(map, 10);
 
     // Start game
     map->thisFrame = clock();
@@ -108,18 +140,83 @@ void UpdateMap(MAP* map)
     if(time - map->thisFrame < map->deltaTime)
         return;
 
-    int some_update = map->player->updated;
-
     // Player update
     if(map->player->updated)
     {
-        map->player->updated = 0;
         map->map[POS(map->player->old_x, map->player->old_y)] = ' ';
         map->map[POS(map->player->x, map->player->y)] = map->player->face;
     }
 
-    if(some_update) PrintMap(map);
+    int guardUpdate = UpdateGuards(map);
+
+    int someUpdate = map->player->updated || guardUpdate;
+    if(someUpdate) PrintMap(map);
+
     map->thisFrame = clock();
+    map->player->updated = 0;
+}
+
+int UpdateGuards(MAP* map)
+{
+    int update = 0;
+    for(int g = 0; g < map->totalGuards; g++)
+    {
+        map->allGuards[g]->frameCounter++;
+
+        if(map->allGuards[g]->frameCounter >= map->allGuards[g]->speed) // Move
+        {
+            update = 1;
+
+            map->allGuards[g]->old_x = map->allGuards[g]->x;
+            map->allGuards[g]->old_y = map->allGuards[g]->y;
+
+            switch(map->allGuards[g]->direction){
+                case NORD:
+                    map->allGuards[g]->y--;
+                    if(map->allGuards[g]->y < 0){
+                        // Invert direction
+                        map->allGuards[g]->direction *= -1;
+                        map->allGuards[g]->y = map->allGuards[g]->old_y + 1;
+                    }
+                    break;
+
+                case SOUTH:
+                    map->allGuards[g]->y++;
+                    if(map->allGuards[g]->y >= MAX_H){
+                        // Invert direction
+                        map->allGuards[g]->direction *= -1;
+                        map->allGuards[g]->y = map->allGuards[g]->old_y - 1;
+                    }
+                    break;
+
+                case EAST:
+                    map->allGuards[g]->x++;
+                    if(map->allGuards[g]->x >= MAX_W){
+                        // Invert direction
+                        map->allGuards[g]->direction *= -1;
+                        map->allGuards[g]->x = map->allGuards[g]->old_x - 1;
+                    }
+                    break;
+
+                case WEST:
+                    map->allGuards[g]->x--;
+                    if(map->allGuards[g]->x < 0){
+                        // Invert direction
+                        map->allGuards[g]->direction *= -1;
+                        map->allGuards[g]->x = map->allGuards[g]->old_x + 1;
+                    }
+                    break;
+
+                default: break;
+            }
+
+            map->map[POS(map->allGuards[g]->old_x, map->allGuards[g]->old_y)] = ' ';
+            map->map[POS(map->allGuards[g]->x, map->allGuards[g]->y)] = map->allGuards[g]->face;
+            map->allGuards[g]->frameCounter = 0;
+        }
+    }
+
+    return update;
 }
 
 void UpdatePosition(PLAYER* p, int key_pressed)
@@ -183,6 +280,37 @@ void AddPlayer(MAP* map, PLAYER* player)
     map->map[POS(map->player->x, map->player->y)] = map->player->face;
 }
 
+GUARD* RandomGuard()
+{
+    GUARD* g = malloc(sizeof(GUARD));
+
+    g->old_x = g->x = RandomIntFrom0ToMax(MAX_W);
+    g->old_y = g->y = RandomIntFrom0ToMax(MAX_H);
+    g->direction = RandomInt(1, 3);
+
+    g->speed = RandomInt(10, 15);
+    g->face = 'G';
+    g->frameCounter = 0;
+
+    return g;
+}
+
+void CreateRandomGuards(MAP* map, int totalGuards)
+{
+    map->totalGuards = totalGuards;
+    map->allGuards = malloc(totalGuards*sizeof(*map->allGuards));
+
+    for(int g = 0; g < totalGuards; g++)
+    {
+        GUARD* guard = RandomGuard();
+
+        // Add the new guard
+        map->allGuards[g] = guard;
+
+        map->map[POS(guard->x, guard->y)] = guard->face;
+    }
+}
+
 MAP* InitializeMap(int width, int heigth)
 {
     MAP* map = malloc(sizeof(MAP));
@@ -204,6 +332,10 @@ MAP* InitializeMap(int width, int heigth)
 
 void FreeMap(MAP* map)
 {
+    for(int i = 0; i < map->totalGuards; i++)
+        free(map->allGuards[i]);
+    free(map->allGuards);
+
     free(map->map);
     free(map);
 }
